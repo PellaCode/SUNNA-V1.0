64723d48f11b2827ac5a0e1c8bea418ca0d8a462
PellaCode <woodworkingleaf@gmail.com>
2025-11-08 19:47:18 +0100

    Update queue.py

--- player/queue.py
+++ player/queue.py
@@ -1,1 +1,17 @@
+from collections import deque
 
+class PlayQueue:
+    def __init__(self):
+        self._queue = deque()
+
+    def add(self, item: str):
+        self._queue.append(item)
+
+    def next(self):
+        return self._queue.popleft() if self._queue else None
+
+    def clear(self):
+        self._queue.clear()
+
+    def peek_all(self):
+        return list(self._queue)
